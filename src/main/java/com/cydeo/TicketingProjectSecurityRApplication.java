68435672d1d7c9418d982fc19e95dca524f5a854
rom201 <kovalchukr1104@gmail.com>
2023-07-10 18:56:28 -0400

    Initial commit

---
+++ src/main/java/com/cydeo/TicketingProjectSecurityRApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingProjectSecurityRApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketingProjectSecurityRApplication.class, args);
+	}
+
+}
